a602d33a5ac9b799a4c7dd33f87cc63b6c6d2650
woues1 <toni.hirvikallio@gmail.com>
2024-10-28 12:22:10 +0200

    week-2 monday-lab6 task completed

---
+++ week-2/week2-fe-lab6/src/Footer.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+function Footer(props) {
+  return <footer>{props.text}</footer>;
+}
+
+export default Footer;
---
+++ week-2/week2-fe-lab6/src/Sidebar.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+function Sidebar(props) {
+  return <aside>{props.content}</aside>;
+}
+
+export default Sidebar;
